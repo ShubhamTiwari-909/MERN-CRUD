83a736f972811af5ec51f7460c256be6f31760d2
Shubham Tiwari <shubham.tiwari@devronins.com>
2022-02-16 10:49:34 +0530

    User Already exist login in the mongo db code

--- Backend/Queries.js
+++ Backend/Queries.js
@@ -1,110 +1,105 @@
 const mongo = require("./mongo");
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
 const userSchema = require("./Schema/Schema");
-
-const app = express();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const port = process.env.PORT || 3001;
 
+const app = express();
+
 app.use(cors());
-app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
-const getData = () => {
-  return userSchema.find({});
-};
-
-
-
-//get method
+// get method
 app.get("/", (req, res) => {
-  const connectToMongo = async (logic) => {
-    await mongo().then(async (response) => {
+  const connnectToMongo = async () => {
+    await mongo().then(async () => {
       try {
-        console.log("connected succefully");
-        const result = await logic();
+        const result = await userSchema.find({});
         res.send(result);
       } finally {
-        console.log("done fetching the data");
+        console.log("Data Fetched succefully");
       }
     });
   };
-
-  connectToMongo(getData);
+  connnectToMongo();
 });
-
-
 
 //post method
 app.post("/post", (req, res) => {
-  const saveData = () => {
-    const user = {
-      email: req.body.email,
-      username: req.body.username,
-      password: req.body.password,
-    };
-    return new userSchema(user).save();
-  };
-
-  const connectToMongo = async (logic) => {
+  const connnectToMongo = async () => {
     await mongo().then(async () => {
       try {
-        console.log("connected succefully");
-        const result = await logic();
-        console.log(result);
-        res.send(result);
+        const userAlreadyExist = req.body.username;
+        const user = {
+          email: req.body.email,
+          username: req.body.username,
+          password: req.body.password,
+        };
+        const userExist = await userSchema
+          .find({ username: userAlreadyExist })
+          .count();
+        if (userExist >= 1) {
+          // res.send(404);
+        } else {
+          const result = await userSchema(user).save();
+          res.send(result);
+        }
       } finally {
-        console.log("done");
+        console.log("Data Saved succefully");
       }
     });
   };
-
-  connectToMongo(saveData);
+  connnectToMongo();
 });
 
-
-
-// delete method
+//delete method
 app.delete("/delete/:id", (req, res) => {
-  const connectToMongo = async () => {
+  const connnectToMongo = async () => {
     await mongo().then(async () => {
       try {
         const id = req.params.id;
-        console.log(id);
-        const result = await userSchema.remove({ _id: id });
-        console.log(result);
+        const result = await userSchema.deleteOne({ _id: id });
         res.send(result);
       } finally {
-        console.log("deleted");
+        console.log("Data Deleted succefully");
       }
     });
   };
-
-  connectToMongo();
+  connnectToMongo();
 });
 
-
-
-//update method
+//delete method
 app.put("/update", (req, res) => {
-  const connectToMongo = async () => {
+  const connnectToMongo = async () => {
     await mongo().then(async () => {
       try {
         const updateId = req.body.updateId;
         const updateEmail = req.body.updateEmail;
+        const updateUsername = req.body.updateUsername;
+        const updatePassword = req.body.updatePassword;
 
-        const result = await userSchema.update({'_id' : updateId} , { $set : {'email' : updateEmail}})
+        const result = await userSchema.update(
+          { _id: updateId },
+          {
+            $set: {
+              email: updateEmail,
+              username: updateUsername,
+              password: updatePassword,
+            },
+          }
+        );
         res.send(result);
       } finally {
-        console.log("updated email");
+        console.log("Data Deleted succefully");
       }
     });
   };
-  connectToMongo();
+  connnectToMongo();
 });
 
 app.listen(port, () => {
-  console.log(`running on ${port}`);
+  console.log(`running on port ${port}`);
 });
--- Backend/mongo.js
+++ Backend/mongo.js
@@ -1,11 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Database = "mongodb+srv://ShubhamDev:Shubham909@cluster0.zkttc.mongodb.net/Demo?retryWrites=true&w=majority";
+const Database =
+  "mongodb+srv://ShubhamDev:Shubham909@cluster0.zkttc.mongodb.net/Demo?retryWrites=true&w=majority";
 
 module.exports = async () => {
-    await mongoose.connect(Database , { 
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    return mongoose
-}
+  await mongoose.connect(Database, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  return mongoose
+};
+
